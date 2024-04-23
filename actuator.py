416fff02717a5ba9ff222db06aec66b23dbb0365
JL Bualoy <jan@joinsafe.app>
2024-04-23 11:24:18 +0800

    Added pedestrian count and multiplier functionality

--- actuator.py
+++ actuator.py
@@ -1,56 +1,244 @@
-from ultralytics import YOLO
-from ultralytics.solutions import object_counter
+# Ultralytics YOLO 🚀, AGPL-3.0 license
+
+import argparse
+from collections import defaultdict
+from pathlib import Path
+
 import cv2
-import pyfirmata
-import time
+import numpy as np
+from shapely.geometry import Polygon
+from shapely.geometry.point import Point
 
-board = pyfirmata.Arduino('/dev/tty.usbmodem1101')
-print("Communication Successfully started")
+from ultralytics import YOLO
+from ultralytics.utils.files import increment_path
+from ultralytics.utils.plotting import Annotator, colors
 
-model = YOLO("yolov8n.pt")
-cap = cv2.VideoCapture("traffic-view/AUP-1.mp4")
-assert cap.isOpened(), "Error reading video file"
-w, h, fps = (int(cap.get(x)) for x in (cv2.CAP_PROP_FRAME_WIDTH, cv2.CAP_PROP_FRAME_HEIGHT, cv2.CAP_PROP_FPS))
+track_history = defaultdict(list)
 
-line_points = [(20, 400), (1080, 400)]  # line or region points
-classes_to_count = [0, 1, 2, 3, 5, 7] 
+current_region = None
+counting_regions = [
+    {
+        "name": "Pedestrian Region",
+        "polygon": Polygon([(50, 80), (250, 20), (450, 80), (400, 350), (100, 350)]), # Polygon points
+        "counts": 0,
+        "dragging": False,
+        "region_color": (255, 155, 100),  # BGR Value
+        "text_color": (255, 255, 255),  # Region Text Color
+    },
+    {
+        "name": "Multiplier Region",
+        "polygon": Polygon([(200, 250), (440, 250), (440, 800), (200, 800)]),  # Polygon points
+        "counts": 0,
+        "dragging": False,
+        "region_color": (100, 200, 255),  # BGR Value
+        "text_color": (0, 0, 0),  # Region Text Color
+    },
+]
 
-# Video writer
-video_writer = cv2.VideoWriter("object_counting_output.avi",
-                    cv2.VideoWriter_fourcc(*'mp4v'),
-                    fps,
-                    (w, h))
 
-# Init Object Counter
-counter = object_counter.ObjectCounter()
-counter.set_args(view_img=True,
-                reg_pts=line_points,
-                classes_names=model.names,
-                draw_tracks=True,
-                line_thickness=2)
+def mouse_callback(event, x, y, flags, param):
 
-while True:
+    global current_region
+
+    # Mouse left button down event
+    if event == cv2.EVENT_LBUTTONDOWN:
+        for region in counting_regions:
+            if region["polygon"].contains(Point((x, y))):
+                current_region = region
+                current_region["dragging"] = True
+                current_region["offset_x"] = x
+                current_region["offset_y"] = y
+
+    # Mouse move event
+    elif event == cv2.EVENT_MOUSEMOVE:
+        if current_region is not None and current_region["dragging"]:
+            dx = x - current_region["offset_x"]
+            dy = y - current_region["offset_y"]
+            current_region["polygon"] = Polygon(
+                [(p[0] + dx, p[1] + dy) for p in current_region["polygon"].exterior.coords]
+            )
+            current_region["offset_x"] = x
+            current_region["offset_y"] = y
+
+    # Mouse left button up event
+    elif event == cv2.EVENT_LBUTTONUP:
+        if current_region is not None and current_region["dragging"]:
+            current_region["dragging"] = False
+
+
+def run(
+    weights="yolov8n.pt",
+    source=None,
+    device="cpu",
+    view_img=False,
+    save_img=False,
+    exist_ok=False,
+    line_thickness=2,
+    track_thickness=2,
+    region_thickness=4,
+):
     
-    while cap.isOpened():
-        success, im0 = cap.read()
+    vid_frame_count = 0
+
+    # Check source path
+    if not Path(source).exists():
+        raise FileNotFoundError(f"Source path '{source}' does not exist.")
+
+    # Setup Model
+    model = YOLO(f"{weights}")
+    model.to("cuda") if device == "0" else model.to("cpu")
+
+    # Extract classes names
+    names = model.model.names
+
+    # Video setup
+    videocapture = cv2.VideoCapture(source)
+    frame_width, frame_height = int(videocapture.get(3)), int(videocapture.get(4))
+    fps, fourcc = int(videocapture.get(5)), cv2.VideoWriter_fourcc(*"mp4v")
+
+    # Output setup
+    save_dir = increment_path(Path("ultralytics_rc_output") / "exp", exist_ok)
+    save_dir.mkdir(parents=True, exist_ok=True)
+    video_writer = cv2.VideoWriter(str(save_dir / f"{Path(source).stem}.mp4"), fourcc, fps, (frame_width, frame_height))
+
+    # Iterate over video frames
+    while videocapture.isOpened():
+        success, frame = videocapture.read()
         if not success:
-            print("Video frame is empty or video processing has been successfully completed.")
             break
-        tracks = model.track(im0, persist=True, show=False,
-                            classes=classes_to_count)
+        vid_frame_count += 1
 
-        im0 = counter.start_counting(im0, tracks)
-        #video_writer.write(im0)
-        
-        board.digital[12].write(1)
-        board.digital[8].write(1)
-        board.digital[7].write(1)
-        time.sleep(1)
-        board.digital[12].write(0)
-        board.digital[8].write(0)
-        board.digital[7].write(0)
-        time.sleep(1)
+        # Extract the results
+        results = model.track(frame, persist=True, classes=None)
 
-    cap.release()
-    #video_writer.release()
+        if results[0].boxes.id is not None:
+            boxes = results[0].boxes.xyxy.cpu()
+            track_ids = results[0].boxes.id.int().cpu().tolist()
+            clss = results[0].boxes.cls.cpu().tolist()
+
+            annotator = Annotator(frame, line_width=line_thickness, example=str(names))
+            
+            classes_with_multiplier = {'bicycle': 0, 'bus': 0, 'car': 0, 'motorcycle': 0, 'truck': 0}
+
+            for box, track_id, cls in zip(boxes, track_ids, clss):
+                
+                annotator.box_label(box, str(names[cls]), color=colors(cls, True))
+                bbox_center = (box[0] + box[2]) / 2, (box[1] + box[3]) / 2  # Bbox center
+
+                track = track_history[track_id]  # Tracking Lines plot
+                track.append((float(bbox_center[0]), float(bbox_center[1])))
+                if len(track) > 30:
+                    track.pop(0)
+                points = np.hstack(track).astype(np.int32).reshape((-1, 1, 2))
+                cv2.polylines(frame, [points], isClosed=False, color=colors(cls, True), thickness=track_thickness)
+                
+                # Check if detection inside rectangle region
+                for region in counting_regions:
+                    if region["name"] == "Multiplier Region" and region["polygon"].contains(Point((bbox_center[0], bbox_center[1]))):
+                        # Apply multipliers based on class
+                        if cls == 0:  # bicycle
+                            classes_with_multiplier['bicycle'] += 0.5
+                        elif cls == 1:  # bus
+                            classes_with_multiplier['bus'] += 1.5
+                        elif cls == 2:  # car
+                            classes_with_multiplier['car'] += 1
+                        elif cls == 3:  # motorcycle
+                            classes_with_multiplier['motorcycle'] += 0.7
+                        elif cls == 5:  # truck
+                            classes_with_multiplier['truck'] += 2
+                
+                # Check if detection inside polygon region 
+                for region in counting_regions:
+                    if region["name"] == "Pedestrian Region" and cls == 4 and region["polygon"].contains(Point((bbox_center[0], bbox_center[1]))):
+                        region["counts"] += 1
+
+
+                total_time = sum(classes_with_multiplier.values())
+
+        # Draw regions (Polygons/Rectangles)
+        for region in counting_regions:
+            region_label = ""
+            if region["name"] == "Pedestrian Region":
+                # Only show label for 'person' counts in polygon region
+                if region["counts"] > 0:
+                    region_label = f"Person Count: {region['counts']}"
+                else:
+                    region_label = "Person Count: 0"
+            else:
+                # Show total time label for other regions
+                region_label = f"Time Added: {total_time:.2f}"
+
+            region_color = region["region_color"]
+            region_text_color = region["text_color"]
+
+
+            polygon_coords = np.array(region["polygon"].exterior.coords, dtype=np.int32)
+            centroid_x, centroid_y = int(region["polygon"].centroid.x), int(region["polygon"].centroid.y)
+
+            text_size, _ = cv2.getTextSize(
+                region_label, cv2.FONT_HERSHEY_SIMPLEX, fontScale=0.7, thickness=line_thickness
+            )
+            text_x = centroid_x - text_size[0] // 2
+            text_y = centroid_y + text_size[1] // 2
+            cv2.rectangle(
+                frame,
+                (text_x - 5, text_y - text_size[1] - 5),
+                (text_x + text_size[0] + 5, text_y + 5),
+                region_color,
+                -1,
+            )
+            cv2.putText(
+                frame, region_label, (text_x, text_y), cv2.FONT_HERSHEY_SIMPLEX, 0.7, region_text_color, line_thickness
+            )
+            cv2.polylines(frame, [polygon_coords], isClosed=True, color=region_color, thickness=region_thickness)
+
+        if view_img:
+            if vid_frame_count == 1:
+                cv2.namedWindow("Optimized Traffic Light Inference Mode")
+                cv2.setMouseCallback("Optimized Traffic Light Inference Mode", mouse_callback)
+            cv2.imshow("Optimized Traffic Light Inference Mode", frame)
+
+        if save_img:
+            video_writer.write(frame)
+
+        for region in counting_regions:  # Reinitialize count for each region
+            region["counts"] = 0
+
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+
+    del vid_frame_count
+    video_writer.release()
+    videocapture.release()
     cv2.destroyAllWindows()
+
+
+def parse_opt():
+    """Parse command line arguments."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--weights", type=str, default="yolov8n.pt", help="initial weights path")
+    parser.add_argument("--device", default="", help="cuda device, i.e. 0 or 0,1,2,3 or cpu")
+    parser.add_argument("--source", type=str, required=True, help="video file path")
+    parser.add_argument("--view-img", action="store_true", help="show results")
+    parser.add_argument("--save-img", action="store_true", help="save results")
+    parser.add_argument("--exist-ok", action="store_true", help="existing project/name ok, do not increment")
+    parser.add_argument("--line-thickness", type=int, default=2, help="bounding box thickness")
+    parser.add_argument("--track-thickness", type=int, default=2, help="Tracking line thickness")
+    parser.add_argument("--region-thickness", type=int, default=4, help="Region thickness")
+
+    return parser.parse_args()
+
+
+def main(opt):
+    """Main function."""
+    run(**vars(opt))
+
+
+if __name__ == "__main__":
+    opt = parse_opt()
+    main(opt)
+
+
+# python actuator.py --source "traffic-view/AUP-1.mp4" --weights "runs/detect/train3/weights/best.pt" --view-img
+
+# {0: 'bicycle', 1: 'bus', 2: 'car', 3: 'motorcycle', 4: 'person', 5: 'truck'}
